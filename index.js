e011fe0b988e20490a415848716abab501739bd8
michaelwhittemore <michaelwhittemore@gmail.com>
2019-06-17 20:37:08 -0700

    fixed spelling mistakes, prevented values from being adjusted before games, and slight balance changes

--- index.js
+++ index.js
@@ -1,63 +1,79 @@
 //color constants
-const safe='#14A76C';
-const mid='#FFE400';
-const danger='#FF652F';
+const safe = '#14A76C';
+const mid = '#FFE400';
+const danger = '#FF652F';
 $(document).ready(
     () => {
         //intialize variables
         let angery = 0;
         let chonky = 0;
         let annoyance = 0;
-        let berserk = false
-        $("#confused").hide();
         let score = 0;
         let bestScore = 0;
         let gameInterval;
         let confuseInterval;
         //functions to modify variable and the appriopraite text
-        const colorCorrector= (name,value) =>{
-            if (value<30){
-                $('#'+name).css("color",safe)
-            } else if (value<70){
-                $('#'+name).css("color",mid)
-            } else if (value>=70){
-                $('#'+name).css("color",danger)
+        const colorCorrector = (name, value) => {
+            if (value < 30) {
+                $('#' + name).css("color", safe)
+            } else if (value < 70) {
+                $('#' + name).css("color", mid)
+            } else if (value >= 70) {
+                $('#' + name).css("color", danger)
             }
         }
         const adjustAngery = (amount) => {
             angery += amount;
             angery = angery < 0 ? 0 : angery
             $('#angery').text(angery)
-            colorCorrector('angery',angery)
+            colorCorrector('angery', angery)
+            if (angery >= 100) {
+                alert("The Kitty Has Gone Berserk And Eats YOU! \nGame Over")  
+                gameOver()
+            }
         }
         const adjustChonky = (amount) => {
             chonky += amount;
             chonky = chonky < 0 ? 0 : chonky
             $('#chonky').text(Math.round(chonky))
-            colorCorrector('chonky',chonky)
+            colorCorrector('chonky', chonky)
+            if (chonky >= 100) {
+                alert("The Kitty Is Too Full And Explodes! \nGame Over");
+                gameOver()
+            }
         }
         const adjustAnnoyance = (amount) => {
             annoyance += amount;
             annoyance = annoyance < 0 ? 0 : annoyance
             $('#annoyance').text(annoyance)
-            colorCorrector('annoyance',annoyance)
+            colorCorrector('annoyance', annoyance)
+            if (annoyance >= 100) {
+                alert("You Were So Annoyed That You Threw The Kitty Out The Window! \nGame Over");
+                gameOver()
+            }
         }
         const gameOver = () => {
-            angery = 0;
-            chonky = 0;
-            annoyance = 0;
-            berserk = false;
+            //reset values
+            angery = chonky = annoyance = 0;
+            adjustAngery(0); adjustAnnoyance(0); adjustChonky(0);
             $("#confused").hide()
             clearInterval(gameInterval)
             clearTimeout(confuseInterval)
-            bestScore= score > bestScore ? score : bestScore;
+            bestScore = score > bestScore ? score : bestScore;
             $("#bestScore").text(bestScore)
             score = 0;
+            //make the buttons disabled
             $('#start').prop('disabled', false);
+            $('#reset').prop('disabled', true);
+            $('#pet').prop('disabled', true);
+            $('#pickUp').prop('disabled', true);
+            $('#sing').prop('disabled', true);
+            $('#feed').prop('disabled', true);
+
         }
         //buttons
         $("#feed").click(() => {
-            adjustChonky(5);
+            adjustChonky(8);
             adjustAngery(-10);
             adjustAnnoyance(-5);
         })
@@ -69,46 +85,34 @@ $(document).ready(
             adjustAnnoyance(-10);
             adjustAngery(20);
         })
-        $('#sing').click(()=>{
+        $('#sing').click(() => {
             $("#confused").show()
-            confuseTimeout=setTimeout( ()=> {
+            confuseTimeout = setTimeout(() => {
                 $("#confused").hide()
-            },3000)
+            }, 3000)
         })
         $("#reset").click(() => {
             gameOver()
-            
+
         })
-        $("#start").click(()=>{
+        $("#start").click(() => {
             gameInterval = setInterval(gameloop, 200)
             $('#start').prop('disabled', true);
+            $('#reset').prop('disabled', false);
+            $('#pet').prop('disabled', false);
+            $('#pickUp').prop('disabled', false);
+            $('#sing').prop('disabled', false);
+            $('#feed').prop('disabled', false);
+
+
         })
         //gameloop
         const gameloop = () => {
             adjustAngery(1)
-            if (berserk) {
-                adjustAnnoyance(5)
-            } else {
-                adjustAnnoyance(1)
-            }
+            adjustAnnoyance(1)
             adjustChonky(-.5)
-            if (chonky >= 100) {
-                alert("The Kitty Is Too Full And Explodes! \nGame Over");
-                gameOver()
-            }
-            if (annoyance >= 100) {
-                alert("You Were So Annoyed You Throw The Kitty Out The Window! \nGame Over");
-                gameOver()
-            }
-            if (angery >= 100) {
-                berserk = true;
-                $("#beserk").text("The kitty has gone beserk!")
-            } else {
-                berserk = false;
-                $("#beserk").text("")
-            }
             score++;
             $("#score").text(score)
         }
-        
+
     })
